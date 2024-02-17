8c7ba7ab8f2b2a64530488a9204fbf99786adba7
SerhiiOberemchuk <gardenitparts@gmail.com>
2024-02-17 17:46:24 +0100

    first commit

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,61 @@
+class Key {
+  private signature: string;
+  constructor() {
+    this.signature = Math.random().toString();
+  }
+
+  getSignature(): string {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log("Door is opened! Person has entered the house.");
+    } else {
+      console.log("The door is closed! Person can`t enter the house.");
+    }
+  }
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super(key);
+  }
+
+  openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+      console.log("Door is now open!");
+    } else {
+      console.log("Error! Failed to open the door!");
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +64,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
